d81d4c0739a6db56fa82b6faed820e071deeff23
Damien FERNANDES <fernandd@iut-acy.local>
2017-02-14 13:41:48 +0100

    Ajout d'une TextView pour débeugage du panier et ajout de la somme des prix de la liste

--- app/src/main/java/com/fernandes/damien/barhelp/ListItemActivity.java
+++ app/src/main/java/com/fernandes/damien/barhelp/ListItemActivity.java
@@ -5,14 +5,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public class ListItemActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
     private ArrayList<Conso> listItem = new ArrayList<>();
     private GridView listConso;
     private ArrayList<Conso> panier = new ArrayList<>();
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +30,8 @@ public class ListItemActivity extends AppCompatActivity implements AdapterView.O
         listConso = (GridView) findViewById(R.id.gridview);
         listConso.setOnItemClickListener(this);
 
+        text = (TextView) findViewById(R.id.textView);
+        text.setText("");
         listConso.setAdapter(new GridViewAdapter(getApplicationContext(), R.layout.activity_list_item, listItem));
 
 
@@ -33,7 +39,13 @@ public class ListItemActivity extends AppCompatActivity implements AdapterView.O
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Double somme = 0.0;
         panier.add(listItem.get(position));
         Toast.makeText(getApplicationContext(), "1 "+ listItem.get(position).getName()+" ajouté au panier", Toast.LENGTH_SHORT).show();
+        //text.setText(text.getText()+listItem.get(position).getName()+"\n");*
+        for(Conso uneConso:panier)
+            somme=somme+uneConso.getPrix();
+        DecimalFormat df = new DecimalFormat("#.####");
+        text.setText(String.format("%.2f", somme)+ " €");
     }
 }
