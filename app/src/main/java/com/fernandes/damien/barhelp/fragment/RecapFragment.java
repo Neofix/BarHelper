bfa2aaa73c69e5f5cc42095744e9333212ce270e
Neofix <damien.fernandes@etu.univ-smb.fr>
2017-03-18 14:05:15 +0100

    Ajout des fragments(vides) pour l'orientation paysage)

---
+++ app/src/main/java/com/fernandes/damien/barhelp/fragment/RecapFragment.java
@@ -0,0 +1,10 @@
+package com.fernandes.damien.barhelp.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by damien on 18/03/17.
+ */
+
+public class RecapFragment extends Fragment {
+}
